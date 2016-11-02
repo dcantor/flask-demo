5682e5d664218fbcfd124f1e832f1bb232e27cec
daniel.cantor@gmail.com <friday11>
2016-11-01 22:20:57 -0400

    -intial commit

--- demo1/main.py
+++ demo1/main.py
@@ -3,8 +3,10 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Method used: %s" % request.method
+@app.route("/<user>")
+def index(user=None):
+    return render_template("user.html", user=user)
+
 
 @app.route("/bacon", methods=['GET', 'POST'])
 def bacon():
@@ -17,6 +19,11 @@ def bacon():
 def profile(name):
     return render_template("profile.html", name=name)
 
+
+@app.route("/shopping")
+def shopping():
+    food = ["Cheese", "Tuna", "Beef", "Toothpaste"]
+    return render_template("shopping.html", food=food)
 
 if __name__ == "__main__":
     app.run()
